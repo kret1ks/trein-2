d3bb37801228fc9a54c930c15b6f91dffc3a8695
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-12 21:23:21 +0200

    123

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -1,2 +1,119 @@
-const listEl = document.querySelector("list")
-listEl
+// const myButtonEl = document.querySelector("#myButton")
+// const messageEl = document.querySelector("#message")
+// // console.log(myButtonEl);
+
+
+// myButtonEl.addEventListener("click", function(){
+//     messageEl.textContent = "Привіт Світ!!"
+// })   
+
+
+
+
+
+
+
+ // ТВОЄ ЗАВДАННЯ:
+        // 1. Знайди елемент з текстом (id="text")
+        // 2. Знайди кнопку (id="btn")
+        // 3. Зроби так, щоб при кліку на кнопку
+        //    текст змінювався на "Привіт JavaScript!"
+
+
+
+
+    // const textEl = document.querySelector("#text")
+    // const btnEl = document.querySelector("#btn")
+
+    // btnEl.addEventListener("click", function(){
+    //     textEl.textContent = "Привіт JavaScript!"
+    // })
+
+
+
+
+
+
+      // ЗАВДАННЯ:
+        // 1. Перша кнопка має змінювати текст (ти це вже вмієш)
+        // 2. Друга кнопка має змінювати колір тексту на синій
+
+
+
+        // const textEl = document.querySelector("#text");
+        // const btnEL = document.querySelector("#btn");
+        // const colorBtnEl = document.querySelector("#colorBtn");
+        
+
+        // btnEL.addEventListener("click", function(){
+        //     textEl.textContent = "Привіт JavaScript!"
+            
+        // })
+
+
+        // colorBtnEl.addEventListener("click", function(){
+        //     textEl.style.color = "blue"
+        // })
+
+
+
+
+
+        // ЗАВДАННЯ:
+        // 1. Знайди всі елементи (лічильник, кнопки)
+        // 2. Зроби так, щоб:
+        //    - Кнопка "+1" збільшувала лічильник на 1
+        //    - Кнопка "-1" зменшувала лічильник на 1
+        // 3. Лічильник не повинен йти нижче 0!
+
+
+
+        const counterEl = document.querySelector("#counter")
+        const incrementEl = document.querySelector("#increment")
+        const decrement = document.querySelector("#decrement")
+
+
+        incrementEl.addEventListener("click", function(){
+            let currenValue = Number(counterEl.textContent)
+            counterEl.textContent = currenValue + 1;
+        })
+
+        decrement.addEventListener("click", function(){
+            let currentValue = Number(counterEl.textContent)
+            if(currentValue > 0){
+                counterEl.textContent = currentValue - 1
+            }
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // incrementEl.addEventListener("click", function(){
+        //     let currentValue = Number(counterEl.textContent)
+        //     counterEl.textContent = currentValue + 1;
+        // })
+
+        // decrement.addEventListener("click", function(){
+        //     let currentValue = Number(counterEl.textContent)
+        //     if(currentValue > 0){
+        //         counterEl.textContent = currentValue - 1
+        //     }
+        // })
